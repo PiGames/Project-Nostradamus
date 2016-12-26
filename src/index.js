e7e9aa5f53e7cbe23d893466f32c13a3bd8edd5b
pietrzakacper <kacpietrzak@gmail.com>
2016-12-26 20:54:09 +0100

    Setting up eslint

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import GameState from 'states/GameState';
 
 class Game extends Phaser.Game {
 
-	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
-	}
+  constructor() {
+    super( 500, 500, Phaser.AUTO, 'content', null );
+    this.state.add( 'GameState', GameState, false );
+    this.state.start( 'GameState' );
+  }
 
 }
 
--- src/objects/RainbowText.js
+++ src/objects/RainbowText.js
@@ -1,38 +1,38 @@
 class RainbowText extends Phaser.Text {
 
-	constructor(game, x, y, text) {
+  constructor( game, x, y, text ) {
 
-		super(game, x, y, text, { font: "45px Arial", fill: "#ff0044", align: "center" });
+    super( game, x, y, text, { font: '45px Arial', fill: '#ff0044', align: 'center' } );
 
-		this._speed = 125; //ms
-		this._colorIndex = 0;
-		this._colors = ['#ee4035', '#f37736', '#fdf498', '#7bc043', '#0392cf'];
+    this._speed = 125;
+    this._colorIndex = 0;
+    this._colors = [ '#ee4035', '#f37736', '#fdf498', '#7bc043', '#0392cf' ];
 
-		this.colorize();
-		this.startTimer();
+    this.colorize();
+    this.startTimer();
 
-		this.game.stage.addChild(this);
+    this.game.stage.addChild( this );
 
-	}
-	
-	startTimer() {
-		this.game.time.events.loop(this._speed, this.colorize, this).timer.start();
-	}
+  }
 
-	colorize() {
+  startTimer() {
+    this.game.time.events.loop( this._speed, this.colorize, this ).timer.start();
+  }
 
-		for (let i = 0; i < this.text.length; i++) {
+  colorize() {
 
-			if (this._colorIndex === this._colors.length) {
-				this._colorIndex = 0;
-			}
+    for ( let i = 0; i < this.text.length; i++ ) {
 
-			this.addColor(this._colors[this._colorIndex], i);
-			this._colorIndex++;
+      if ( this._colorIndex === this._colors.length ) {
+        this._colorIndex = 0;
+      }
 
-		}
+      this.addColor( this._colors[ this._colorIndex ], i );
+      this._colorIndex++;
 
-	}
+    }
+
+  }
 
 }
 
