a5e039ecfcae7a4b3f7ad2ddb095dd9b843796e8
bibixx <bartosz@legiec.eu>
2016-12-27 19:22:26 +0100

    Changed maint tile color. Added source files for tiles. Added protection against closing window.
    Protection against leaving page is commented out since it was driving me
    crazy that i had to confirm leaving every time browsersync fired.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import ProjectNostradamus from './ProjectNostradamus';
 
 new ProjectNostradamus( '100%', '100%', Phaser.AUTO, 'content' );
+
+/*
+!!! This is protection against leaving page while still in game. It is commented out since it was driving me crazy that i had to confirm leavinmg every time browsersync fired. !!!
+window.onbeforeunload = (e) => {
+  return 'Really want to quit the game?';
+};
+
+document.onkeydown = ( e ) => {
+  e = e || window.event;
+  if ( e.ctrlKey ) {
+    const c = e.which || e.keyCode;
+    switch ( c ) {
+    case 83:
+    case 87:
+      e.preventDefault();
+      e.stopPropagation();
+      break;
+    }
+  }
+};
+*/
