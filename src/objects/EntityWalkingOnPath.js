9df0dc06ce346512a3d6cc35b47010411d0bc180
pietrzakacper <kacpietrzak@gmail.com>
2017-01-05 21:47:22 +0100

    Implementing precalculated paths into entity movement and reactoring code

--- src/objects/EntityWalkingOnPath.js
+++ src/objects/EntityWalkingOnPath.js
@@ -3,100 +3,66 @@ import PathFinder from '../objects/PathFinder.js';
 import { ZOMBIE_SPEED, ZOMBIE_LOOKING_OFFSET, MIN_DISTANCE_TO_TARGET } from '../constants/ZombieConstants';
 import { pixelsToTileX, pixelsToTileY, tileToPixels } from '../utils/MapUtils.js';
 
-/** Create Entity that is supposed to walk on given path. */
+/** Create Entity that is supposed to walk on given path. Set position of entity on first given target*/
 export default class EntityWalkingOnPath extends Entity {
-  constructor( game, x, y, imageKey, frame, path, walls ) {
-    super( game, x, y, imageKey, frame );
+  constructor( game, imageKey, frame, targets, walls ) {
+    const position = tileToPixels( targets[ 0 ] );
+
+    super( game, position.x, position.y, imageKey, frame );
 
     this.pathfinder = new PathFinder();
     this.pathfinder.setGrid( walls );
 
-    this.pathTargets = path;
+    this.targets = targets;
 
     this.pathsBetweenPathTargets = [];
-
-    this.currentTarget = this.pathTargets[ 0 ];
 
     /* disable update until path is calculated */
     this.isMoving = false;
 
-    //this.calculateStandardPaths();
+    this.currentPathIndex = 0;
+    this.currentStepIndex = 0;
 
-    this.startMovement();
+    this.calculateStandardPaths( () => {
+      this.stepTarget = this.pathsBetweenPathTargets[ this.currentPathIndex ].path[ this.currentStepIndex ];
+      this.enableMovement = true;
+    } );
   }
   /**Recursive function that calculates standard paths and save them into pathsBetweenPathTargets container.  Recurse approach is used to handle asynchronous nature of findPath method */
-  calculateStandardPaths( index = 0 ) {
-    if ( this.pathsBetweenPathTargets.length === this.pathTargets.length ) {
+  calculateStandardPaths( doneCallback, index = 0 ) {
+    if ( this.pathsBetweenPathTargets.length === this.targets.length ) {
+      doneCallback();
       return;
     }
 
-    const start = this.pathTargets[ index ];
-    const target = ( index === this.pathTargets.length - 1 ) ? this.pathTargets[ 0 ] : this.pathTargets[ index + 1 ];
+    const start = this.targets[ index ];
+    const target = ( index === this.targets.length - 1 ) ? this.targets[ 0 ] : this.targets[ index + 1 ];
 
     this.pathfinder.findPath( start.x, start.y, target.x, target.y, ( path ) => {
-      this.savePath( path, start, target );
-      this.calculateStandardPaths( index + 1 );
+      this.pathsBetweenPathTargets.push( { path, start, target } );
+      this.calculateStandardPaths( doneCallback, index + 1 );
     } );
-  }
-  savePath( path, start, target ) {
-    this.pathsBetweenPathTargets.push( { path, start, target } );
-  }
-  startMovement() {
-    this.calculatePath( path => this.onPathCalculatingFinish( path ) );
-  }
-  calculatePath( callback ) {
-    this.pathfinder.findPath( pixelsToTileX( this.position.x ), pixelsToTileY( this.position.y ), this.currentTarget.x, this.currentTarget.y, callback );
-  }
-  onPathCalculatingFinish( path ) {
-    this.pathToCurrentTarget = path;
-    this.stepTarget = this.pathToCurrentTarget[ 1 ];
-
-    /* enable update */
-    this.isMoving = true;
   }
   /** Check if current target or step target is reached. Move body in stepTarget direction. */
   update() {
-    if ( this.isMoving ) {
-      if ( this.isReached( this.currentTarget ) ) {
-        this.onCurrentTargetReach();
-        return;
-      }
+    if ( this.enableMovement ) {
       if ( this.isReached( this.stepTarget ) ) {
         this.onStepTargetReach();
         return;
       }
-      /* This can be done better, by comparing body's distance to target in each frame, if it is greater than in previous frame, following line should be called  */
       this.game.physics.arcade.moveToObject( this, tileToPixels( this.stepTarget ), ZOMBIE_SPEED );
 
       this.updateLookDirection();
     }
   }
-  isReached( target ) {
-    const distanceToTarget = this.game.physics.arcade.distanceBetween( this, tileToPixels( target ) );
-    return distanceToTarget <= MIN_DISTANCE_TO_TARGET;
-  }
   onStepTargetReach() {
     this.resetVelocity();
-    this.stepTarget = this.getNextStepTarget();
-    this.game.physics.arcade.moveToObject( this, tileToPixels( this.stepTarget ), ZOMBIE_SPEED );
-  }
-
-  getNextStepTarget() {
-    const stepTargetIndex = this.pathToCurrentTarget.indexOf( this.stepTarget );
-    return this.pathToCurrentTarget[ stepTargetIndex + 1 ];
-  }
-  onCurrentTargetReach() {
-    this.resetVelocity();
-    this.currentTarget = this.getNextCurrentTarget();
-
-    /* disable update until path is calculated */
-    this.isMoving = false;
-
-    this.startMovement();
-  }
-  getNextCurrentTarget() {
-    const currentTargetIndex = this.pathTargets.indexOf( this.currentTarget );
-    return currentTargetIndex >= this.pathTargets.length - 1 ? this.pathTargets[ 0 ] : this.pathTargets[ currentTargetIndex + 1 ];
+    this.currentStepIndex++;
+    if ( this.currentStepIndex === this.pathsBetweenPathTargets[ this.currentPathIndex ].path.length ) {
+      this.currentPathIndex = ( this.currentPathIndex + 1 === this.pathsBetweenPathTargets.length ) ? 0 : this.currentPathIndex + 1;
+      this.currentStepIndex = 0;
+    }
+    this.stepTarget = this.pathsBetweenPathTargets[ this.currentPathIndex ].path[ this.currentStepIndex ];
   }
   updateLookDirection() {
     const lookTarget = tileToPixels( this.stepTarget );
@@ -106,5 +72,9 @@ export default class EntityWalkingOnPath extends Entity {
     const lookY = lookTarget.y + ZOMBIE_LOOKING_OFFSET * ( lookTarget.y - this.position.y );
 
     this.lookAt( lookX, lookY );
+  }
+  isReached( target ) {
+    const distanceToTarget = this.game.physics.arcade.distanceBetween( this, tileToPixels( target ) );
+    return distanceToTarget <= MIN_DISTANCE_TO_TARGET;
   }
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -14,8 +14,7 @@ export default class Game extends Phaser.State {
     this.game.camera.follow( this.player );
 
     this.zombies = this.game.add.group();
-    this.zombies.add( new Zombie( this.game, TILE_WIDTH * 1 + TILE_WIDTH / 2, TILE_HEIGHT + TILE_HEIGHT / 2, 'zombie', PLAYER_INITIAL_FRAME, [ { x: 1, y: 4 }, { x: 10, y: 10 } ], this.map.getWallsPostions() ) );
-    //this.zombies.add( new Zombie( this.game, TILE_WIDTH * 3 + TILE_WIDTH / 2, TILE_HEIGHT + TILE_HEIGHT / 2, 'zombie', PLAYER_INITIAL_FRAME, [ { x: 2, y: 2 }, { x: 10, y: 10 } ], this.map.getWallsPostions() ) );
+    this.zombies.add( new Zombie( this.game, 'zombie', PLAYER_INITIAL_FRAME, [ { x: 1, y: 4 }, { x: 10, y: 10 } ], this.map.getWallsPostions() ) );
   }
 
   update() {
